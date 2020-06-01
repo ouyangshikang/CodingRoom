6ec4d76cec593a2430b0e18a483421911e0e92ef
baituo <baituo@gaoding.com>
2020-06-01 23:09:24 +0800

    feat: ts enum

---
+++ Typescript/basic/enum.ts
@@ -0,0 +1,38 @@
+/**
+ * 数字枚举
+ * 通过反向映射，可以用枚举名称和值来索引
+ */
+enum Role {
+    Reporter = 1,
+    Developer,
+    Maintainer = 7,
+    Owner,
+    Guest
+}
+// console.log(Role);
+
+/**
+ * 字符串枚举
+ * 只能通过枚举名称来索引
+ */
+enum Message {
+    Suceess = '成功',
+    Fail = '失败'
+}
+// console.log(Message);
+
+/**
+ * 枚举成员
+ * 1. 常量枚举
+ * 2. 计算枚举
+ */
+enum Char {
+    a,
+    b = Char.a,
+    c = 1 + 2,
+    d = Math.random(),
+    e = '1234'.length,
+    f = 22
+}
+console.log(Char);
+
